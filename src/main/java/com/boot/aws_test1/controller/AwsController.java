fa19e0fac10644700f0099a5bdba7ae712e10a53
nwj4831 <nwj4831@naver.com>
2024-08-26 14:30:40 +0900

    0826

---
+++ src/main/java/com/boot/aws_test1/controller/AwsController.java
@@ -0,0 +1,16 @@
+package com.boot.aws_test1.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class AwsController {
+    @GetMapping("/aws/v1")
+    public String hello(){
+        log.info("/aws/v1 이 호출되었어요");
+
+        return "<h1>aws v1 호출</h1>";
+    }
+}
